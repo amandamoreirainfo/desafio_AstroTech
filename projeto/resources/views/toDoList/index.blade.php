1db152be663e46996352479bc0e09694cd1325be
amandamoreirainfo <amandamoreirainfo@gmail.com>
2024-02-27 13:44:09 -0300

    added login,register and logout

--- projeto/resources/views/toDoList/index.blade.php
+++ projeto/resources/views/toDoList/index.blade.php
@@ -5,18 +5,26 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
 
     <form action="{{ route('toDoLists.store') }}" method="post">
         @csrf
-        <input type="text" name="title">
-        <button type="submit">Confirmar</button>
+        <div class="form">
+            <h1>To Do List</h1>
+            <div class="teste"></div>
+            <div class="formContainer">
+                <input type="text" placeholder="Digite um texto" id="title" name="title">
+                <button  type="submit">
+                    +
+                </button>
+            </div>
+
+        </div>
     </form>
 
     <div class="container">
-
-        <h1>ToDo List</h1>
 
         <table class="table table-striped">
             <thead>
--- projeto/routes/web.php
+++ projeto/routes/web.php
@@ -11,12 +11,21 @@ use App\Http\Controllers\ToDoListController;
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
-|
+|T
 */
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
+
+
 
 Route::get('/toDoLists', [ToDoListController::class, 'index'])->name('toDoLists.index');
 Route::post('/toDoLists', [ToDoListController::class, 'store'])->name('toDoLists.store');
